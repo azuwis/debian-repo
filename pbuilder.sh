fb98d4bcf09c5687a718cd8aa5365bb04f538976
Zhong Jianxin (azuwis) <azuwis@gmail.com>
2011-04-18 01:57:10 +0800

    init

---
+++ pbuilder.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+if [ x"$DISTS" = x ]; then
+	DISTS="squeeze lenny"
+fi
+if [ x"$ARCHS" = x ]; then
+	ARCHS="amd64 i386"
+fi
+
+case "$1" in
+	create | update)
+		for i in $DISTS
+		do
+			for j in $ARCHS
+			do
+				sudo DIST=$i ARCH=$j pbuilder $1
+			done
+		done
+		;;
+	build | gbp)
+		for i in $DISTS
+		do
+			pbuilder_opt=""
+			for j in $ARCHS
+			do
+				if [ x"$1" = x"gbp" ]; then
+					DIST=$i ARCH=$j git-buildpackage --git-ignore-new --git-builder="pdebuild $pbuilder_opt" --git-cleaner="/bin/true"
+				else
+					DIST=$i ARCH=$j pdebuild $pbuilder_opt
+				fi
+				pbuilder_opt="-- --binary-arch"
+			done
+		done
+		;;
+	*)
+		echo "Usage: $0 {create|update|build|gbp}"
+		exit 1
+		;;
+esac
