3c11dcc41bf9829d16bef73d55ed78f0b5213cb8
Zhong Jianxin (azuwis) <azuwis@gmail.com>
2013-11-23 11:19:23 +0800

    [WIP] Refactor
    - use cowbuilder to build
    - use eatmydata to speed up build
    - use options to speficy build ARCH and DIST
    - use tmux to run multiple build

--- pbuilder.sh
+++ pbuilder.sh
@@ -1,259 +1,342 @@
 #!/bin/bash
-
-export REPREPRO_BASE_DIR=/srv/debian-repo/reprepro
-REPREPRO_STAGING_DIR=/srv/debian-repo/reprepro-staging
-PBUILDER_RESULT_DIR=/var/cache/pbuilder/result
+#
+# Script ease building Debian packages
 
 set -e
-if [ x"$DISTS" = x ]; then
-	DISTS="squeeze wheezy"
-fi
-if [ x"$ARCHS" = x ]; then
-	ARCHS="amd64 i386"
-fi
 
-# sort DISTS, workaround for reprepro missing *.orig.tar.gz when processincoming
+# Helper functions
+err() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" >&2
+}
+load_var() {
+  local i
+  for i in ${PBUILDERRC_FILES}; do
+    if [ -e "$i" ]; then
+      eval `grep "^$1" $i`
+    fi
+  done
+}
+tmux_run() {
+  tmux split-window -d "$@; echo; read -p '#### finished ####' tmp"
+  tmux select-layout even-vertical >& /dev/null
+}
+usage() {
+  cat <<EOF
+USAGE: $0 [-haiswS] subcommand
+
+OPTIONS:
+  -h help
+  -a amd64
+  -i i386
+  -s squeeze
+  -w wheezy
+  -S sid
+
+SUBCOMMAND:
+  create
+  update
+  login [--save-after-login]
+  build|gbp [src_dir ...]
+  post-build
+  repo
+  log [pattern]
+  commit
+  push
+  tag
+  watch list_file
+  staging
+  new-patch
+EOF
+  exit 1
+}
+
+# Global settings
+PBUILDERRC_FILES="/etc/pbuilderrc ~/.pbuilderrc"
+# git-pbuilder
+load_var REPO_BASE
+load_var COWBUILDER_BASE
+export COWBUILDER_BASE
+GIT_PBUILDER_OPTIONS="-nc"
+# reprepro
+export REPREPRO_BASE_DIR="${REPO_BASE}/reprepro"
+REPREPRO_STAGING_DIR="${REPO_BASE}/reprepro-staging"
+load_var BUILDRESULT
+# other
+DEFAULT_DISTS="squeeze wheezy"
+DEFAULT_ARCHS="amd64 i386"
+
+# Options
+DISTS=""
+ARCHS=""
+OPTIND=1
+while getopts "haiswS" opt; do
+  case "$opt" in
+    h) usage ;;
+    a) ARCHS="$ARCHS amd64" ;;
+    i) ARCHS="$ARCHS i386" ;;
+    s) DISTS="$DISTS squeeze" ;;
+    w) DISTS="$DISTS wheezy" ;;
+    S) DISTS="$DISTS sid" ;;
+    *) usage ;;
+  esac
+done
+shift $((OPTIND-1))
+
+# Set default if empty
+DISTS=${DISTS:-$DEFAULT_DISTS}
+ARCHS=${ARCHS:-$DEFAULT_ARCHS}
+
+# Sort DISTS and ARCHS, workaround for reprepro missing *.orig.tar.gz when
+# processincoming
 DISTS=`echo $DISTS | tr " " "\n" | sort | tr "\n" " "`
 ARCHS=`echo $ARCHS | tr " " "\n" | sort | tr "\n" " "`
 
-DATE=`date +%Y%m%d%H%M%S`
-
-NCPU=`grep "^processor" /proc/cpuinfo | wc -l`
-
-build_all()
-{
-	SOURCE=`dpkg-parsechangelog | awk '/^Source: / {print $2}'`
-	VERSION=`dpkg-parsechangelog | awk '/^Version: / {print $2}'`
-	jobidx=1
-	failed=0
-	pidlist=""
-	inc_orig="--debbuildopts -sa"
-	echo "+++ start building, see ../*.build for log +++"
-	for i in $DISTS
-	do
-		build_bin_only=""
-		for j in $ARCHS
-		do
-			echo "[$jobidx] building for $i $j"
-			PDEBUILD="pdebuild --logfile ../${SOURCE}_${VERSION}_${DATE}_${i}_${j}.build $inc_orig $build_bin_only"
-			if [ x"$action" = x"gbp" ]; then
-				DIST=$i ARCH=$j DEB_BUILD_OPTIONS="nocheck parallel=$((NCPU/2))" git-buildpackage --git-submodules --git-ignore-new --git-builder="$PDEBUILD" --git-cleaner='/bin/true' >&/dev/null &
-				pidlist="$pidlist $!"
-			else
-				DIST=$i ARCH=$j $PDEBUILD >&/dev/null &
-				pidlist="$pidlist $!"
-			fi
-			inc_orig=""
-			build_bin_only="-- --binary-arch"
-			let "jobidx+=1"
-			sleep 5
-		done
-	done
-
-	jobidx=1
-	for job in $pidlist
-	do
-		echo -n "waiting job [$jobidx]..."
-		if wait $job; then
-			echo "success"
-		else
-			let "failed+=1"
-			echo "failed"
-		fi
-		let "jobidx+=1"
-	done
-
-	if [ $failed -gt 0 ]; then
-		echo "$failed build(s) failed, please check build log"
-		return 1
-	fi
+# Exit if not inside debian source dir
+check_dir() {
+  if [ ! -e "debian/changelog" ]; then
+    err "Please run inside debian source dir."
+    exit 1
+  fi
 }
 
-post-build()
-{
-	echo "+++ installing build results +++"
-	repo
-	echo "+++ git clean up +++"
-	clean
-	echo "+++ git taging +++"
-	tag
+# Init build host env
+prepare() {
+  sudo apt-get install git-buildpackage cowbuilder reprepro bsd-mailx sudo tmux
 }
 
-clean()
-{
-	if [ -f debian/changelog ]; then
-		git reset --hard HEAD && git clean -df
-	else
-		echo "skip clean, not in debian source dir"
-	fi
+# Update/create/login
+ucl() {
+  action="$1"
+  shift
+  local DIST ARCH
+  for DIST in ${DISTS}; do
+    load_var OTHERMIRROR
+    for ARCH in ${ARCHS}; do
+      # cowbuilder will not install extrapackages when creating .cow
+      # run update after create to get extrapackages installed
+      if [ x"$action" == x"create" ]; then
+        tmux_run "DIST=${DIST} ARCH=${ARCH} COWBUILDER_BASE=${COWBUILDER_BASE} \
+          git-pbuilder create --othermirror '$OTHERMIRROR' $*; \
+          DIST=${DIST} ARCH=${ARCH} COWBUILDER_BASE=${COWBUILDER_BASE} \
+          git-pbuilder update"
+      else
+        tmux_run "DIST=${DIST} ARCH=${ARCH} COWBUILDER_BASE=${COWBUILDER_BASE} \
+          git-pbuilder ${action} --othermirror '$OTHERMIRROR' $*"
+      fi
+    done
+  done
 }
 
-log()
-{
-	pattern="*.build"
-	if [ -n $1 ]; then
-		pattern="*${1}*.build"
-	fi
-	pager `ls -t $pattern ../$pattern | head -n1`
+# Build
+build() {
+  NCPU=`grep "^processor" /proc/cpuinfo | wc -l`
+  check_dir
+  clean ask
+  # only build in one ARCH for source which only has `Architecture: all' packages
+  if [ x"$(cat debian/control |grep '^Architecture:' | grep -v all)" == x ]; then
+    err "Architecture: all, build only once per DIST."
+    ARCHS=$(echo ${ARCHS} | awk '{print $1}')
+  fi
+  inc_orig="-sa"
+  for DIST in ${DISTS}; do
+    bin_only=""
+    for ARCH in ${ARCHS}; do
+      tmux_run "DEB_BUILD_OPTIONS='nocheck parallel=$((NCPU/2))' \
+        COWBUILDER_BASE=${COWBUILDER_BASE} time -p git-buildpackage \
+        --git-export-dir='../build_${DIST}_${ARCH}' \
+        --git-dist=${DIST} --git-arch=${ARCH} \
+        ${inc_orig} ${bin_only} $*"
+      inc_orig="-sd"
+      bin_only="-b"
+    done
+  done
 }
 
-tag()
-{
-	git-buildpackage --git-ignore-new --git-tag-only
+download() {
+  git-import-dsc --download "$@"
 }
 
-commit()
-{
-	VERSION=`dpkg-parsechangelog | awk '/^Version: / {print $2}'`
-	git add debian/changelog && git commit -m "debian/changelog: $VERSION"
+build_all() {
+  SOURCE=`dpkg-parsechangelog | awk '/^Source: / {print $2}'`
+  VERSION=`dpkg-parsechangelog | awk '/^Version: / {print $2}'`
+  jobidx=1
+  failed=0
+  pidlist=""
+  inc_orig="--debbuildopts -sa"
+  echo "+++ start building, see ../*.build for log +++"
+  for i in $DISTS
+  do
+    build_bin_only=""
+    for j in $ARCHS
+    do
+      echo "[$jobidx] building for $i $j"
+      PDEBUILD="pdebuild --logfile ../${SOURCE}_${VERSION}_${DATE}_${i}_${j}.build $inc_orig $build_bin_only"
+      if [ x"$action" = x"gbp" ]; then
+        DIST=$i ARCH=$j DEB_BUILD_OPTIONS="nocheck parallel=$((NCPU/2))" git-buildpackage --git-submodules --git-ignore-new --git-builder="$PDEBUILD" --git-cleaner='/bin/true' >&/dev/null &
+        pidlist="$pidlist $!"
+      else
+        DIST=$i ARCH=$j $PDEBUILD >&/dev/null &
+        pidlist="$pidlist $!"
+      fi
+      inc_orig=""
+      build_bin_only="-- --binary-arch"
+      let "jobidx+=1"
+      sleep 5
+    done
+  done
+
+  jobidx=1
+  for job in $pidlist
+  do
+    echo -n "waiting job [$jobidx]..."
+    if wait $job; then
+      echo "success"
+    else
+      let "failed+=1"
+      echo "failed"
+    fi
+    let "jobidx+=1"
+  done
+
+  if [ $failed -gt 0 ]; then
+    echo "$failed build(s) failed, please check build log"
+    return 1
+  fi
 }
 
-push()
-{
-	git push
-	git push --tags
+post-build() {
+  echo "+++ installing build results +++"
+  repo
+  echo "+++ git clean up +++"
+  clean
+  echo "+++ git taging +++"
+  tag
 }
 
-repo()
-{
-	for i in $DISTS
-	do
-		reprepro gensnapshot $i prev
-	done
-	reprepro processincoming default
-	for i in $DISTS
-	do
-		reprepro gensnapshot $i $DATE
-	done
+clean() {
+  check_dir
+  if [ $(git status --porcelain | wc -l) -eq 0 ]; then 
+    return
+  fi
+  if [ x"$1" == x"ask" ]; then
+    git status --porcelain
+    read -p "Clear git working tree?(Ctrl-C to cancel)" tmp
+  fi
+  git reset --hard HEAD && git clean -df
 }
 
-staging()
-{
-	mkdir -p ${PBUILDER_RESULT_DIR}/staging
-	cp ${PBUILDER_RESULT_DIR}/* ${PBUILDER_RESULT_DIR}/staging || true
-	reprepro -b ${REPREPRO_STAGING_DIR} processincoming default
+log() {
+  pattern="*.build"
+  if [ -n $1 ]; then
+    pattern="*${1}*.build"
+  fi
+  pager `ls -t $pattern ../$pattern | head -n1`
 }
 
-new-patch()
-{
-	git checkout -- debian/patches/
-	num_patches=`cat debian/patches/series | wc -l`
-	pushd debian/patches >& /dev/null
-	for i in *.patch
-	do
-		if [[ ${i:0:4} =~ ^[0-9]+$ ]] && [ ${i:0:4} -gt $num_patches ]; then
-			git add $i
-			echo $i >> series
-			git add series
-			tmp=${i:5}
-			msg=${tmp/%.patch/}
-			git commit -m "debian/patches/: $msg"
-		fi
-	done
-	popd >& /dev/null
-	git clean -f
+tag() {
+  git-buildpackage --git-ignore-new --git-tag-only
 }
 
-watch()
-{
-	# source_pkg_name local_dist debian_dist
-
-	if [ x"$1" = x"" ]; then
-		exit 1
-	else
-		watch_list=$1
-	fi
-	cat $watch_list | while read debian_source local_dist debian_dist
-	do
-		local_version=`reprepro --list-format '${version}' listfilter $local_dist '$Source (== '$debian_source'), $Architecture (==source)'`
-		if [ x"$debian_dist" = x"" ]; then
-			debian_dist=$local_dist
-		fi
-		debian_version=`curl -s http://packages.debian.org/source/${debian_dist}/${debian_source} | grep "Source Package: ${debian_source}" | awk -F'(\(|\))' '{print $2}'`
-		if [ x"$debian_version" != x"" ]; then
-			if dpkg --compare-versions "$debian_version" gt "$local_version" || [ x"$DEBUG" != x"" ]; then
-				echo "${debian_source}: debian(${debian_dist} ${debian_version}) local(${local_dist} ${local_version})"
-			fi
-		else
-			echo "${debian_source}: can't get upstream version"
-		fi
-	done
+# New changelog
+nc() {
+  VERSION=`dpkg-parsechangelog | awk '/^Version: / {print $2}'`
+  git add debian/changelog && git commit -m "debian/changelog: $VERSION"
 }
 
-action=$1
+push() {
+  git push
+  git push --tags
+}
 
+repo() {
+  for i in $DISTS
+  do
+    reprepro gensnapshot $i prev
+  done
+  reprepro processincoming default
+  for i in $DISTS
+  do
+    reprepro gensnapshot $i $DATE
+  done
+}
+
+staging() {
+  mkdir -p ${PBUILDER_RESULT_DIR}/staging
+  cp ${PBUILDER_RESULT_DIR}/* ${PBUILDER_RESULT_DIR}/staging || true
+  reprepro -b ${REPREPRO_STAGING_DIR} processincoming default
+}
+
+# New patch
+np() {
+  gbp-pq export
+  git checkout -- debian/patches/
+  num_patches=`cat debian/patches/series | wc -l`
+  pushd debian/patches >& /dev/null
+  for i in *.patch; do
+    if [[ ${i:0:4} =~ ^[0-9]+$ ]] && [ ${i:0:4} -gt $num_patches ]; then
+      git add $i
+      echo $i >> series
+      git add series
+      tmp=${i:5}
+      msg=${tmp/%.patch/}
+      git commit -m "debian/patches/: $msg"
+    fi
+  done
+  popd >& /dev/null
+  git clean -f
+}
+
+watch() {
+  # source_pkg_name local_dist debian_dist
+  if [ x"$1" = x"" ]; then
+    usage
+  else
+    watch_list=$1
+  fi
+  cat $watch_list | grep -v '^#' | while read debian_source local_dist debian_dist
+  do
+    local_version=`reprepro --list-format '${version}' listfilter $local_dist '$Source (== '$debian_source'), $Architecture (==source)'`
+    if [ x"$debian_dist" = x"" ]; then
+      debian_dist=$local_dist
+    fi
+    debian_version=`curl -s http://packages.debian.org/source/${debian_dist}/${debian_source} | grep "Source Package: ${debian_source}" | awk -F'(\(|\))' '{print $2}'`
+    if [ x"$debian_version" != x"" ]; then
+      if dpkg --compare-versions "$debian_version" gt "$local_version" || [ x"$DEBUG" != x"" ]; then
+        echo "${debian_source}: debian(${debian_dist} ${debian_version}) local(${local_dist} ${local_version})"
+      fi
+    else
+      echo "${debian_source}: can't get upstream version"
+    fi
+  done
+}
+
+action="$1"
 case "$action" in
-	create | update)
-		jobidx=1
-		pidlist=""
-		log_dir=`mktemp -d`
-		echo "+++ starting $action, see $log_dir for logs +++"
-		for i in $DISTS
-		do
-			for j in $ARCHS
-			do
-				echo "[$jobidx] $action for $i $j"
-				sudo DIST=$i ARCH=$j pbuilder $action --othermirror "deb http://repo.163.org/debian ${i} main contrib non-free" >& $log_dir/${i}_${j}_${action}.log &
-				pidlist="$pidlist $!"
-				let "jobidx+=1"
-			done
-		done
-
-		jobidx=1
-		failed=0
-		for job in $pidlist
-		do
-			echo -n "waiting job [$jobidx]..."
-			if wait $job; then
-				echo "success"
-			else
-				let "failed+=1"
-				echo "failed"
-			fi
-			let "jobidx+=1"
-		done
-
-		if [ $failed -gt 0 ]; then
-			echo "$failed $action(s) failed, please check $action log in $log_dir"
-		else
-			echo "$action all done, cleaning log"
-			rm -r $log_dir
-		fi
-		;;
-	build | gbp)
-		shift
-		if [ $# -gt 0 ]; then
-			for dir in $@
-			do
-				if [ -d "$dir" ]; then
-					pushd "$dir"
-					build_all
-					popd
-				fi
-			done
-		else
-			build_all
-		fi
-		;;
-	*)
-		if [ $(type -t $action) == "function" ]; then
-			shift
-			$action "$@"
-		else
-			echo "Usage: $0 subcommand"
-			echo "    create"
-			echo "    update"
-			echo "    build|gbp [src_dir ...]"
-			echo "    post-build"
-			echo "    repo"
-			echo "    log [pattern]"
-			echo "    commit"
-			echo "    push"
-			echo "    tag"
-			echo "    watch list_file"
-			echo "    staging"
-			echo "    new-patch"
-			exit 1
-		fi
-		;;
+update|create|login)
+  ucl "$@"
+  exit $?
+  ;;
+build)
+  shift
+  if [ $# -gt 0 ]; then
+    for dir in $@
+    do
+      if [ -d "$dir" ]; then
+        pushd "$dir"
+        build
+        popd
+      fi
+    done
+  else
+    build
+  fi
+  ;;
+*)
+  if [ x$(type -t $action) == x"function" ]; then
+    shift
+    $action "$@"
+  else
+    usage
+  fi
+  ;;
 esac
